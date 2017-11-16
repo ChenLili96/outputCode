ec6dd3083ad81fbce180c06d6d80a9b9de301b25
ChenLili96 <chel4_15@uni.worc.ac.uk>
2017-11-16 15:37:35 +0000

    test

--- www/js/index.js
+++ www/js/index.js
@@ -2,13 +2,28 @@
 $(document).on("pagecreate","#pageone",function(){
   
   	$('#tapholdtext').on("click",function(){
-        alert("click");
-    	//alert.random();
+        click();
+       
  	});                       
     
 });
 
 
 function random() {
-    return !Math.round(Math,random());
+	return !Math.round(Math.random());
+}
+
+
+function click(){
+    
+    var rand = random();
+    
+    if (rand) {
+        navigator.notification.beep(1);
+        
+    }
+    else {
+        navigator.notification.beep(2);
+       
+    }
 }
